1c9a01e6fac248b420005f239714b467e06e090d
24804764s <24804764s@common.cpce-polyu.edu.hk>
2026-04-15 01:10:47 +0800

    260415_RC_/BoardGameController.php

--- app/Http/Controllers/BoardGameController.php
+++ app/Http/Controllers/BoardGameController.php
@@ -7,8 +7,15 @@ use App\Models\BoardGame;
 class BoardGameController extends Controller
 {
     public function index()
-        {
-            $boardGames = BoardGame::where('is_available', true)->get();
-            return view('board-games', compact('boardGames'));
-        }
+    {
+        $boardGames = BoardGame::where('is_available', true)->get();
+        return view('board-games', compact('boardGames'));
+    }
+
+    // Change this method
+    public function show($id)
+    {
+        $boardGame = BoardGame::findOrFail($id);
+        return view('game_info', compact('boardGame'));
+    }
 }
