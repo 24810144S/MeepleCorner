721e2ee00480457f0e57b8fad96493989b4c67a7
24804807s <24804807s@common.cpce-polyu.edu.hk>
2026-04-17 15:24:53 +0800

    260417_fj_space

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Space extends Model
 {
-    protected $table = 'spaces';  // Explicitly specify table name
+    protected $table = 'spaces';
     
     protected $fillable = [
         'name',
         'type',
         'capacity',
         'description',
+        'image',  
         'is_active',
     ];
     
@@ -40,5 +41,22 @@ class Space extends Model
     public function isPublicTable()
     {
         return $this->type !== 'private';
+    }
+    
+    // Helper to get image URL
+    public function getImageUrlAttribute()
+    {
+        if ($this->image && file_exists(public_path('storage/' . $this->image))) {
+            return asset('storage/' . $this->image);
+        }
+        
+        // Return default image based on table type
+        if ($this->type === 'private') {
+            return asset('img/default-private.jpg');
+        } elseif ($this->type === 'premium') {
+            return asset('img/default-premium.jpg');
+        } else {
+            return asset('img/default-table.jpg');
+        }
     }
 }
